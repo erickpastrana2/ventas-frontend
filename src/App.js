eb53e9c4e57b1199691ad27cc4bc64470d805cb0
Epastrana <erickpastrana@hotmail.com>
2022-05-23 17:44:58 -0500

    first example multiple component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi prro erick</h1>
+      <Header/>
+      <Menu titulo="Eeste es el titulo" subtitulo="Subtitulo"/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    const titulo = "Header2";
+    const a = 5;
+    const b = 6;
+
+    return (
+        <div>
+            <h2>{titulo}</h2>
+            <p>{a + b}</p>
+        </div>
+    )
+}
+
+export default Header;
